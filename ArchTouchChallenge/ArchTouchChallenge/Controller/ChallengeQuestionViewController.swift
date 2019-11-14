31f5bce1d7594573ec11d1224b44bd470194f2a3
Gustavo Cavalcanti <cg.gustavo@gmail.com>
2019-11-14 19:27:20 -0300

    Create an alert manager to show when time is up or when won the game. Add functionality to reset the game.

--- ArchTouchChallenge/ArchTouchChallenge/Controller/ChallengeQuestionViewController.swift
+++ ArchTouchChallenge/ArchTouchChallenge/Controller/ChallengeQuestionViewController.swift
@@ -14,10 +14,15 @@ class ChallengeQuestionViewController: UIViewController {
     @IBOutlet weak var inputWordTextField: UITextField!
     @IBOutlet weak var hitRate: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startResetButton: UIButton!
     
     private var challengeVM: ChallengeQuestionVM?
     private var timerVM: TimerVM?
     fileprivate var loadingAlert: UIViewController?
+    private var isStarted = false
+    
+    private let resetText = "Reset"
+    private let startText = "Start"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +34,29 @@ class ChallengeQuestionViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         challengeVM?.loadData()
+        timerVM?.setInitialTime()
     }
+    
+    @IBAction func clickStartResetButton(_ sender: Any) {
+        if isStarted {
+            startResetButton.setTitle(startText, for: .normal)
+            timerVM?.resetTimer()
+        } else {
+            startResetButton.setTitle(resetText, for: .normal)
+            timerVM?.startCount(completion: {
+                let alert = AlertManager.sharedInstance.createTimeFinishedAlert(totalAnswered: 0, total: 50, actionButton: {
+                    self.startResetButton.setTitle(self.startText, for: .normal)
+                    self.isStarted = false
+                    self.timerVM?.resetTimer()
+                })
+
+                self.present(alert, animated: true, completion: nil)
+            })
+        }
+        
+        isStarted = !isStarted
+    }
+    
 }
 
 // MARK: - ChallengeQuestionVMDelegate
--- ArchTouchChallenge/TimerVM.swift
+++ ArchTouchChallenge/TimerVM.swift
@@ -18,17 +18,23 @@ protocol TimerVMDelegate {
 /// Responsible for managing the time of the challenge and notify when it ends.
 class TimerVM {
     private let delegate: TimerVMDelegate
+    private var timer: Timer?
+    
+    private let totalMin = 1
+    private let totalSec = 0
     
     init(delegate: TimerVMDelegate) {
         self.delegate = delegate
     }
     
     /// Start to decreasing the timer.
-    func startCount() {
-        var minutes = 5
-        var seconds = 0
+    ///
+    /// - Parameter completion: callback when finished.
+    func startCount(completion: @escaping () -> Void) {
+        var minutes = totalMin
+        var seconds = totalSec
         
-        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
             if (seconds == 0) {
                 minutes -= 1
                 seconds = 59
@@ -36,16 +42,41 @@ class TimerVM {
                 seconds -= 1
             }
             
-            let minFormatted = String(format: "%02d", minutes)
-            let secFormatted = String(format: "%02d", seconds)
-            
-            let time = "\(minFormatted):\(secFormatted)"
+            let time = self.getTimeFormatted(minutes: minutes, seconds: seconds)
             
             self.delegate.setTime(time: time)
 
             if minutes == 0 && seconds == 0 {
                 timer.invalidate()
+                completion()
             }
         }
+    }
+    
+    /// Invalidate timer if already started and put it to initial value.
+    func resetTimer() {
+        if let timer = timer {
+            timer.invalidate()
+            setInitialTime()
+        }
+    }
+    
+    /// Put a timer to initial value.
+    func setInitialTime() {
+        let time = self.getTimeFormatted(minutes: totalMin, seconds: totalSec)
+        self.delegate.setTime(time: time)
+    }
+    
+    /// Format the time to have two decimal places, and separated by a colon.
+    ///
+    /// - Parameters:
+    ///   - minutes: value of minutes
+    ///   - seconds: value of seconds
+    /// - Returns: time formatted.
+    private func getTimeFormatted(minutes: Int, seconds: Int) -> String {
+        let minFormatted = String(format: "%02d", minutes)
+        let secFormatted = String(format: "%02d", seconds)
+        
+        return "\(minFormatted):\(secFormatted)"
     }
 }
